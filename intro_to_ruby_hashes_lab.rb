7ebd17208c1e71e75f357c528a1ac7ce57434a4e
calm-collection-3916 <khalinredd@gmail.com>
2020-12-31 01:23:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,6 +31,7 @@ def monopoly_with_third_tier
         {:two_piece_owned => 50},
         {:three_piece_owned => 100},
         {:three_piece_owned => 200}
+        ]
       },
       :names => {}
     }
